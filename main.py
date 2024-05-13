e0457fd28e896f62850efe115958c7fe08c95c5a
mhewedy <mhewedy@elm.sa>
2024-05-13 14:49:29 +0300

    enhancements

--- main.py
+++ main.py
@@ -14,50 +14,70 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
+selected_user_msg = "صاحب الحظ السعيد اليوم هو"
+
 chat_id = os.getenv("CHAT_ID", -4201961515)
 users = []
 
 
 async def ping_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info("ping command received")
     await update.message.reply_text("Pong!")
 
 
-async def capture_names(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def yalla_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info("yalla command received")
+    selected_user = select_user()
+    if selected_user:
+        await update.message.reply_text(selected_user_msg + f" {selected_user}")
+
+
+async def capture_users(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     global users, chat_id
     chat_id = update.message.chat_id
 
-    elected_user = update.effective_user.first_name + " " + update.effective_user.last_name
-    if elected_user not in users: users.append(elected_user)
-    logger.info(f'user {elected_user} added!, {users}')
+    captured_user = update.effective_user.first_name + " " + update.effective_user.last_name
+    if captured_user not in users:
+        users.append(captured_user)
+        logger.info(f'user {captured_user} added!, {users}')
 
 
 async def send_lunch_headsup(context):
+    logger.info("send_lunch_headsup")
     global users
     users = []  # clean
-    await context.bot.send_message(chat_id=chat_id, text="""
-    يلا يا شباب أبدأو ضيفو طلابتكم ...
-     خدو بالكم اللي هيبعت حاجه في الشات من دلوقتي لحد ما يتم اختيار الإسم هيكون من ضمن الناس اللي هتدخل في الاختيار
-    """)
+    await context.bot.send_message(chat_id=chat_id, text="يلا يا شباب أبدأو ضيفو طلابتكم ...")
 
 
 async def send_lunch_election(context):
+    logger.info("send_lunch_election")
+    selected_user = select_user()
+    if selected_user:
+        await context.bot.send_message(chat_id=chat_id, text=selected_user_msg + f" {selected_user}")
+
+
+async def select_user():
     global users
 
     if len(users) > 0:
         selected_user = random.choice(users)
         logger.info(f'selected user is: {selected_user}')
-        await context.bot.send_message(chat_id=chat_id, text=f' سعيد الحظ النهارده هو{selected_user}!')
+        users = []
+        logger.info(f'resetting user list!')
+        return selected_user
     else:
         logger.warning('user list is empty')
+        return None
 
 
 def main() -> None:
     application = Application.builder().token(os.getenv("BOT_TOKEN")).build()
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, capture_names))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, capture_users))
 
     application.add_handler(CommandHandler("ping", ping_command))
+    application.add_handler(CommandHandler("yalla", yalla_command))
 
-    headsup_time = dateutil.parser.parse(os.getenv("HEADS_UP_TIME", "08:45")).time()
+    headsup_time = dateutil.parser.parse(os.getenv("HEADS_UP_TIME", "08:30")).time()
     logger.info(f'heads up time is set to: {headsup_time} UTC')
     application.job_queue.run_daily(send_lunch_headsup, time=headsup_time)
 
